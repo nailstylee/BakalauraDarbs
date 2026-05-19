fb5c573f35b0f6a249fbe576923e04dd3e087783
Nils Sauskins <nilssauskins@gmail.com>
2026-05-20 01:52:32 +0300

    bug

--- text/files.js
+++ text/files.js
@@ -1,7 +1,7 @@
 window.VERIFICATION_FILE_LISTS = window.VERIFICATION_FILE_LISTS || {};
 window.VERIFICATION_FILE_LISTS.text = [
   "d2hm2.png",
-  "55psj8.png",
+  "55PSJ8.png",
   "44EqgYtQ.png",
   "4BV5HEW.png",
 ];
