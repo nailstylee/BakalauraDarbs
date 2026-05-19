f9be01f9cb0293db4aaef19bc1fbeb290a970688
Nils Sauskins <nilssauskins@gmail.com>
2026-05-19 18:59:03 +0300

    bildes

--- slide/files.js
+++ slide/files.js
@@ -2,4 +2,6 @@ window.VERIFICATION_FILE_LISTS = window.VERIFICATION_FILE_LISTS || {};
 window.VERIFICATION_FILE_LISTS.slide = [
   "photo-1.png",
   "photo-2.png",
+  "photo-3.png",
+  "photo-4.png",
 ];
